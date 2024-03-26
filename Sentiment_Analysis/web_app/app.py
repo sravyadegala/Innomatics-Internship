d99d2abebe20657f583d7b548488a5fece72d7f4
Degala Sravya <122505654+sravyadegala@users.noreply.github.com>
2024-03-26 16:04:49 +0530

    Add files via upload

---
+++ Sentiment_Analysis/web_app/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect
+import joblib
+import sklearn
+
+# Create the instance of a class
+my_application = Flask(__name__)
+
+# Create the route and bind them with respective function
+@my_application.route("/")
+def home():
+    return render_template("home.html")
+
+@my_application.route("/prediction", methods=["GET", "POST"])
+def prediction_fun():
+    if request.method == "POST":
+        review = request.form.get("review")
+        # Check if the review is empty
+        if not review.strip():
+            return render_template("home.html", error="Please enter a review.")
+        
+        model = joblib.load("models/lr_model.pkl")
+        prediction = model.predict([review])
+        return render_template("result.html", prediction=prediction)
+    else:
+        # If the method is GET, redirect the user back to the home page
+        return redirect("/")
+
+# Run the application
+if (__name__) == "__main__":
+    my_application.run(debug = True, host="0.0.0.0")
